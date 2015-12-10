fc78ad09817182fa19b1adc47836880fa0b7c5d6
alexandercstephens <alexandercstephens@gmail.com>
2015-12-10 12:05:58 -0600

    added tank movement

---
+++ Assets/TestMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMovement : MonoBehaviour {
+    public float speed = 3.0f;
+    public float rotSpeed = 3.0f;
+
+    CharacterController controller;
+    
+	void Awake () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed, 0);
+        controller.SimpleMove(transform.forward * Input.GetAxis("Vertical") * speed);
+	}
+}
